2d633ba80387934ccb46c39daea9321d32267ab6
Nazmul Hasan <141444914+goingdark007@users.noreply.github.com>
2025-07-20 20:36:57 +0600

    Add files via upload

---
+++ learning3 Dart keyword and syntax.dart
@@ -0,0 +1,13 @@
+void main () {
+
+  /// var final const
+
+  var name = 'dart'; /// type defined by assigned value
+
+  print('Hello, $name'); /// $ is used to access the variable value
+
+  final stringName = 'dart'; /// initialized at run time
+
+  const pi = 3.1416; /// initialized at compile time
+
+}
