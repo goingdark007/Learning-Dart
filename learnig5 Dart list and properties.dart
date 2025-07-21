d6c4b86e080256f515cd493f628e40ed569aeba0
Nazmul Hasan <141444914+goingdark007@users.noreply.github.com>
2025-07-22 04:29:42 +0600

    Add files via upload

---
+++ learnig5 Dart list and properties.dart
@@ -0,0 +1,17 @@
+void main () {
+
+  List<int> numbers = [1, 2, 3];
+
+  List<String> fruits = ['mango', 'apple', 'banana'];
+
+  print(numbers.length); // .length shows the length of the list
+
+  print(numbers.isNotEmpty);
+  // .isNotEmpty checks the list is empty or not returns true or false
+
+  print(numbers[0]); // shows the item at index 0
+
+  print(fruits.first); // .first shows first item in list
+
+  print(fruits.last); // .last shows last item in list
+}
