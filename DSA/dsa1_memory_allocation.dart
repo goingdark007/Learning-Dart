68dfc9fb437afc165004df08e53362a793a414c1
Nazmul Hasan <141444914+goingdark007@users.noreply.github.com>
2025-09-24 23:16:11 +0600

    tweaked formats

--- DSA/dsa1_memory_allocation.dart
+++ DSA/dsa1_memory_allocation.dart
@@ -1,11 +1,13 @@
 void main () {
+
   List<int> list1 = [1, 2, 3, 4, 5];
   List<int> list2 = [];
+
   print('Before adding\n$list2');
   list2 = list1;
   print('After adding\n$list2');
-  list1.add(10);
 
+  list1.add(10);
   /// Even after adding 10 in myList its showing in list2 too
   // Because list2 is not copying the value but pointing to the
   // same memory location of the myList
@@ -18,8 +20,8 @@ void main () {
   // list2.add(value);
   // }
   list2 = [...list1];
-  print('After cascading\n$list2');
 
+  print('After cascading\n$list2');
   list1.add(11);
   /// Even after adding 11 in list1 its not adding in list2
   print('After adding 11\n$list2');
