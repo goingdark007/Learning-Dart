f4ac9b00ad7556d3113fec19ea8ca5cd64ace6a1
Nazmul Hasan <141444914+goingdark007@users.noreply.github.com>
2026-04-23 23:22:10 +0600

    Added recursion and backtracking

---
+++ DSA/dsa9_recursion.dart
@@ -0,0 +1,84 @@
+void main () {
+
+  // do some operation
+
+  int f = fibonacci(6);
+  print(f);
+  // operation
+
+  bool n = isPalindrome('fat', 0, 'fat'.length-1);
+
+  print(n);
+
+  generateSubsets([1, 2, 3]);
+
+}
+
+/// n! = n * (n-1) * (n-2) * (n-3) * ... * 1
+/// Using loop
+int fact(int n){
+
+  int res = 1;
+  for(int i = n; i > 1; i--){
+    res *= i;
+  }
+  return res;
+
+}
+
+/// Using recursion
+int factorial(int n){
+
+  if(n <= 1) return 1;
+
+  return n * factorial(n-1);
+
+}
+
+/// loop replacement
+int addFour({int n = 1}){
+
+  if(n >= 4) return n;
+
+  return addFour(n: n+1);
+
+}
+
+/// Fibonacci series 0, 1, 1, 2, 3, 5, 8, 13, 21 ...
+int fibonacci(int n){
+
+  if(n == 0 || n == 1) return n;
+
+  return fibonacci(n-1) + fibonacci(n-2);
+
+}
+
+/// Palindrome detection using recursion
+bool isPalindrome(String str, int left, int right){
+
+  // base case, responsible for stopping the recursion
+  if(left >= right) return true;
+  if(str[left] != str[right]) return false;
+
+  // recursive case
+  return isPalindrome(str, left+1, right-1);
+
+}
+
+/// Backtracking
+void generateSubsets(List<int> num){
+
+  List<int> res = [];
+
+  void backtrack (int index){
+
+    print(List.from(res));
+
+    for(int i = index; i < num.length; i++ ){
+      res.add(num[i]);
+      backtrack(i+1);
+      res.removeLast();
+    }
+  }
+  backtrack(0);
+}
