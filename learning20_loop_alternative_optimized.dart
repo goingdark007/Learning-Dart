f936fb7638560e739100254386364803da5e7ef3
Nazmul Hasan <141444914+goingdark007@users.noreply.github.com>
2026-04-29 12:49:15 +0600

    Added loop alternatives

---
+++ learning20_loop_alternative_optimized.dart
@@ -0,0 +1,44 @@
+void main() {
+
+  /// This methods are more optimized and provides clean code with readability
+
+  /// .forEach() loop
+  List<int> list1 = [10, 30, 50, 60];
+  List<int> list2 = [];
+
+  list1.forEach((elements) {
+    list2.add(elements + 1);
+  });
+
+  print(list2); // [11, 31, 51, 61]
+
+
+  /// List.generate(), syntax: List.generate(length, (length){return ...})
+  // Optimized
+  List<int> list3 = List.generate(10, (index) => index + 1);
+
+  print(list3); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+  /// .map((e) => ) method which is an alternative of loop and used for list
+  // of maps. It returns a list of Iterables<T>
+  // optimized
+  List<Map<String, dynamic>> myList = [
+    {
+      'id' : 01,
+      'name': 'Nazmul'
+    },
+    {
+      'id': 02,
+      'name' : 'Alex'
+    },
+    {
+      'id': 03,
+      'name' : 'Max'
+    }
+  ];
+
+  // use .toList() method to convert iterables to list
+  var myList2 = myList.map((e)=> e['id']).toList();
+  print(myList2);
+
+}
