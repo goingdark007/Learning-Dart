b0a95bf55ad99c2bb1b44a09809e067341f6af50
Nazmul Hasan <141444914+goingdark007@users.noreply.github.com>
2026-05-01 18:30:50 +0600

    tested backtracking

--- testing.dart
+++ testing.dart
@@ -1,8 +1,29 @@
 void main () {
 
-  List<int> list4 = [1, 3, 4, 5];
+  generateSubsets([1, 2, 3]);
+}
 
-  final total = list4.fold<int>(0, (previousValue, element ) => previousValue + element);
-  print(total);
+/// Backtracking
+void generateSubsets(List<int> num){
 
+  List<int> res = [];
+
+  final myList = [];
+
+  void backtrack (int index){
+
+    print(List.from(res));
+
+    myList.add(List.from(res));
+
+    for(int i = index; i < num.length; i++ ){
+      res.add(num[i]);
+      backtrack(i+1);
+      res.removeLast();
+    }
+
+  }
+  backtrack(0);
+
+  print(myList);
 }
