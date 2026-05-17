afa367f487204b7454b179e1b76768a3a5521bc3
Nazmul Hasan <141444914+goingdark007@users.noreply.github.com>
2026-05-17 19:44:15 +0600

    just tested spread operator

--- testing.dart
+++ testing.dart
@@ -1,5 +1,14 @@
-
 void main() {
 
+  final List<int> list1 = [1, 2, 3, 4];
+
+  final copiedList = [...list1];
+  print(copiedList);
+
+  final list2 = [
+    ...list1.map((e) => e +1)
+  ];
+
+  print(list2);
 
 }
